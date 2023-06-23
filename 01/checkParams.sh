64101a7bf98cd008a15432e3c4ecf63129105800
Dzemka <dzemkadzemka20@gmail.com>
2023-06-23 17:56:46 +0300

    01res

---
+++ 01/checkParams.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function checkParams() {
+    if [ $# -ne 6 ]
+    then
+      echo "The number of parameters must be 6.
+Parameter 1 : path.
+Parameter 2 : count of subfolders.
+Parameter 3 : list of letters for file names (max 7).
+Parameter 4 : the number of files in each created folder.
+Parameter 5 : list of letters used in the file name and extension (name - max 7, extension - max 3).
+Parameter 6 : the size of the files (in kilobytes, but not more than 100)."
+    elif [ ! -d "$1" ]; then
+      echo "Invalid parameter 1 : path not found"
+    elif [ ! "$(echo $2 | grep -E '^[1-9]{1,1}($|[0-9]+$)')" ]; then
+        echo "Invalid parameter 2 : The parameter must consist of integers greater than 0"
+    elif [ ! $(echo $3 | grep -E '^[a-zA-Z]{1,7}$') ]; then
+        echo "Invalid parameter 3 : The parameter must consist of alphabetic characters (1-7 alphabetic word)"
+    elif [ ! $(echo $4 | grep -E '^[1-9]{1,1}($|[0-9]+$)') ]; then
+        echo "Invalid parameter 4 : The parameter must consist of integers greater than 0"
+    elif [ ! $(echo $5 | grep -E '^[a-zA-Z]{1,7}($|[.]{1,1}[a-zA-Z]{1,3}$)') ] ; then
+      echo
+       echo "Invalid parameter 5 : The parameter must consist of alphabetic characters (1-7 alphabetic word, and optional for extension 1-3 alphabetic word"
+    elif [ ! $(echo $6 | grep -E '^[1-9]{1,1}(kb$|[0-9]+kb$)') ] || [ $(echo $6 | sed 's/kb//') -gt 100 ]; then
+        echo "Invalid parameter 6 : The parameter must consist of integers (1-100 in kb) ending in \"kb\""
+    fi
+}
---
+++ 01/main.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+. ./checkParams.sh
+check="$(checkParams $@)"
+
+if [ ! -z "$check" ]; then
+  echo -e "$check"
+  exit
+fi
+
+lengthFolderLetter=${#3} #Get length parameter 3
+LD=${3:0:1} #Get first letter of directory name
+LF=${5:0:1} #Get first letter of filename
+nameDirArray[0]=$LD$LD$LD #fill array first letters
+nameFileArray[0]=$LF$LF$LF #fill array first letters
+
+fileSize=${6//kb/} #Cut out "kb". Example: 100kb -> 100
+fileChars=$((fileSize * 1000 / 2)) #Calculate count chars to put in files
+
+lengthFileLetter=${#5} #Get length parameter 5
+indexDot=`expr index $5 .` #Get dot index
+
+if [ "$indexDot" -gt 1 ] ; then #Get the length before the point
+  fileNameMaxIndex=$((indexDot - 1))
+else
+  fileNameMaxIndex=$lengthFileLetter
+fi
+
+for ((i = 0; i < $2; i++)); do #Loop creating directories
+  if [ "$(df --block-size=GB | grep -E '/$' | awk '{printf("%d", $4)}')" -le 1 ]; then #If size partition "/" less or equal 1GB exit
+    exit
+  fi
+  if [ "$i" -eq 0 ] ; then
+    for (( j = 0; j < "$lengthFolderLetter"; j++ )); do
+        nameDirArray[$j]+=${3:$j:1}
+    done
+  else
+    selectedIndex=$((RANDOM % lengthFolderLetter))
+    nameDirArray[$selectedIndex]+=${3:$selectedIndex:1}
+  fi
+  dirName="$1/$(IFS=; echo "${nameDirArray[*]}")_$(date +"%d%m%y")"
+  if [ -d "$dirName" ]; then #If folder exist, create new name
+    i=$((i-1))
+    continue
+  fi
+
+  mkdir "$dirName"
+
+  for (( j = 0; j < $4; j++ )); do
+      if [ "$(df --block-size=GB | grep -E '/$' | awk '{printf("%d", $4)}')" -le 1 ]; then #If size partition "/" less or equal 1GB exit
+        exit
+      fi
+    if [ "$j" -eq 0 ]; then
+      for (( k = 0; k < lengthFileLetter; k++ )); do
+          nameFileArray[$k]=${5:$k:1}
+      done
+    else
+      selectedIndex=$((RANDOM % fileNameMaxIndex))
+      nameFileArray[$selectedIndex]+=${5:$selectedIndex:1}
+    fi
+    fileName="$(IFS=; echo "${nameFileArray[*]}")_$(date +%d%m%y)"
+  yes | head -$fileChars > "$dirName/$fileName"
+  done
+done
